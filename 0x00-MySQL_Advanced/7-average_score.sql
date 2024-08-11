bd617c797b34cf4712e278496c1e84bc2dd0b043
Sarah Gad <sarahgad298@gmail.com>
2024-08-11 18:29:38 +0300

    added table names

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,24 @@
 -- This script defines a stored Procedure.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
     DECLARE sum DECIMAL(10, 2);
     DECLARE num INT;
     DECLARE aver_score DECIMAL(10, 2);
     SELECT SUM(score) INTO sum
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
     SELECT COUNT(score) INTO num
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
     IF num > 0 THEN
         SET aver_score = sum / num;
     ELSE
         SET aver_score = 0;
     END IF;
     UPDATE users
-    SET average_score = aver_score
-    WHERE id = user_id;
+    SET users.average_score = aver_score
+    WHERE users.id = user_id;
 END $$
 DELIMITER ;
