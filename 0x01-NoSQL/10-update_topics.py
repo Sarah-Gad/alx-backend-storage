6731e99047b49e7448e1bf2843ecd2e6c579b6a2
Sarah-Gad <sarahgad298@gmail.com>
2024-08-13 20:12:12 -0700

    updatemany

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """This fucntion changes all topics of a school"""
-    mongo_collection.update_one(
+    mongo_collection.update_many(
             {'name': name},
             {'$set': {'topics': topics}}
             )
